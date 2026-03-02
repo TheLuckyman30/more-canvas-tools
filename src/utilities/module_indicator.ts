2620692caae2109ed816bc71d6d8b03768f17004
TheLuckyman30 <bnauta0@gmail.com>
2026-03-02 02:07:01 -0500

    Setup indicator on whether or not there is a published assignment in an unpublsihed module

--- src/utilities/module_indicator.ts
+++ src/utilities/module_indicator.ts
@@ -1,7 +1,13 @@
 const UNPUBLISHED_COLOR = "red";
 const PUBLISHED_COLOR = "green";
+let assignmentInUnpubMod = false;
 
-function modifyAssignments(assignments: JQuery<HTMLLIElement>) {
+const WARNING_BOX_HTML = "";
+
+function modifyAssignments(
+  assignments: JQuery<HTMLLIElement>,
+  moduleState: string | undefined,
+) {
   for (const assignment of assignments) {
     const state = $(assignment)
       .find("div.ig-admin > span[data-published]")
@@ -11,25 +17,30 @@ function modifyAssignments(assignments: JQuery<HTMLLIElement>) {
         .children("div.ig-row")
         .css("border-left", `3px solid ${UNPUBLISHED_COLOR}`);
     }
+    if (moduleState === "unpublished" && state === "true") {
+      assignmentInUnpubMod = true;
+    }
   }
 }
 
-function modifyModules(modules: JQuery<HTMLElement>, color: string) {
+function modifyModules(modules: JQuery<HTMLElement>) {
   for (const module of modules) {
-    $(module).children(".ig-header").css("background-color", color);
+    const state = $(module).attr("data-workflow-state");
+    if (state === "unpublished") {
+      $(module)
+        .children(".ig-header")
+        .css("background-color", UNPUBLISHED_COLOR);
+    } else {
+      $(module).children(".ig-header").css("background-color", PUBLISHED_COLOR);
+    }
 
     const assignmentList = $(module).find("div.content > ul.ig-list")[0];
     const assignments = $(assignmentList).children("li");
-    modifyAssignments(assignments);
+    modifyAssignments(assignments, state);
   }
 }
 
 export async function injectModuleIndicator(target: HTMLElement) {
-  const unpublishedModules = $(target).children(
-    '[data-workflow-state="unpublished"]',
-  );
-  const publishedModules = $(target).children('[data-workflow-state="active"]');
-
-  modifyModules(unpublishedModules, UNPUBLISHED_COLOR);
-  modifyModules(publishedModules, PUBLISHED_COLOR);
+  const modules = $(target).children("[data-workflow-state]");
+  modifyModules(modules);
 }
