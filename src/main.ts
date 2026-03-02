36c024f4639f7f0e57aaf8fc0bdf59556f52e803
TheLuckyman30 <bnauta0@gmail.com>
2026-03-02 00:58:37 -0500

    Modules are now recolored based on published/unpublished status

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,22 @@ import STYLESHEETS from "~src/stylesheets";
 import U from "~src/userscript";
 
 const describeFailure = errors.failureDescriber({
-    siteName: SITE.NAME,
-    extensionName: U.name,
-    location: document.location,
+  siteName: SITE.NAME,
+  extensionName: U.name,
+  location: document.location,
 });
 
-async function onStart() {
-    
-    log.log(`${U.name} ${U.version} - Hello world!`)
-}
-
 userscripter.run({
-    id: U.id,
-    name: U.name,
-    initialAction: onStart ,
-    stylesheets: STYLESHEETS,
-    operationsPlan: {
-        operations: OPERATIONS,
-        interval: CONFIG.OPERATIONS_INTERVAL,
-        tryUntil: environment.DOMCONTENTLOADED,
-        extraTries: CONFIG.OPERATIONS_EXTRA_TRIES,
-        handleFailures: failures => failures.forEach(compose(log.error, describeFailure)),
-    },
+  id: U.id,
+  name: U.name,
+  initialAction: () => log.log(`${U.name} ${U.version} - Hello world!`),
+  stylesheets: STYLESHEETS,
+  operationsPlan: {
+    operations: OPERATIONS,
+    interval: CONFIG.OPERATIONS_INTERVAL,
+    tryUntil: environment.DOMCONTENTLOADED,
+    extraTries: CONFIG.OPERATIONS_EXTRA_TRIES,
+    handleFailures: (failures) =>
+      failures.forEach(compose(log.error, describeFailure)),
+  },
 });
--- src/utilities/module_indicator.ts
+++ src/utilities/module_indicator.ts
@@ -1,17 +1,43 @@
 import { Module } from "~src/canvas/interfaces";
 
+function colorPublishedModules(
+  publishedModules: Module[],
+  moduleContainer: HTMLElement,
+) {
+  const headers = publishedModules.map((m) => {
+    return $(moduleContainer).find(`#${m.id}`);
+  });
+  for (const header of headers) {
+    $(header[0]).css("background-color", "green");
+  }
+}
+
+function colorUnpublishedModules(
+  unpublishedModules: Module[],
+  moduleContainer: HTMLElement,
+) {
+  const headers = unpublishedModules.map((m) => {
+    return $(moduleContainer).find(`#${m.id}`);
+  });
+
+  for (const header of headers) {
+    $(header[0]).css("background-color", "red");
+  }
+}
+
 export async function injectModuleIndicator(target: HTMLElement) {
   const token = GM_getValue("CANVAS_TOKEN");
-  const info = await fetch(
+  const response = await fetch(
     "https://canvas.instructure.com/api/v1/courses/14264155/modules",
     { headers: { Authorization: `Bearer ${token}` } },
   );
-  const modules: Module[] = await info.json();
-  const unpublishedModules = modules.filter((m) => !m.published);
-
-  const headers = unpublishedModules.map((m) => {
-    return $(target).find(`#${m.id}`);
+  const allModules: Module[] = await response.json();
+  const publishedModules: Module[] = [];
+  const unpublishedModules = allModules.filter((m) => {
+    if (!m.published) return m;
+    publishedModules.push(m);
   });
 
-  $(headers[0][0]).css("background-color", "red");
+  colorUnpublishedModules(unpublishedModules, target);
+  colorPublishedModules(publishedModules, target);
 }
