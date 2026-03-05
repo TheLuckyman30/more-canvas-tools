78be9f791b218ef4aab3b040999f79ba1084b680
TheLuckyman30 <bnauta0@gmail.com>
2026-03-05 02:08:34 -0500

    Added system for reminders to be added from the speed grader

--- src/utilities/add_reminder.ts
+++ src/utilities/add_reminder.ts
@@ -18,16 +18,25 @@ const DATE_INPUT_HTML = `
 <input id="mct-date-picker" type="date"/>
 `;
 
+const SUBMIT_BUTTON_HTML = `
+<button id="mct-submit-reminder">
+    Submit
+</button>
+`;
+
 const INPUT_CONTAINER_HTML = `
 <div id="mct-input-container"
     style="position: fixed; height: 100%; width: 100%; backdrop-filter: blur(4px); background-color: rgba(255, 255, 255, 0.2); z-index: 99; top: 0; bottom: 0; right: 0; left: 0; display: flex; justify-content: center; align-items: center">
-    <div style="width: 20rem; height: 15rem; background-color: #E5E7EB; box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04); padding: 0.5rem; border-radius: 0.375rem;">
+    <div style="display: flex; flex-direction: column; width: 20rem; height: 15rem; background-color: #E5E7EB; box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04); padding: 0.5rem; border-radius: 0.375rem;">
         <div style="display: flex; justify-content: space-between; font-size: 1.5rem">
             <span>Set Your Reminder:</span>
             ${CLOSE_BUTTON}
         </div>
         <div style="display: flex; justify-content: center; align-items: center; height: 100%; width: 100%">
             ${DATE_INPUT_HTML}
+        </div>
+        <div style="width: 100%; display: flex; justify-content: center">
+            ${SUBMIT_BUTTON_HTML}
         </div>
     </div>
 </div>
@@ -49,20 +58,27 @@ export async function injectAddReminder(target: HTMLElement) {
   $("div#mct-grader-reminder").on("click", () => {
     $(target).append(INPUT_CONTAINER_HTML);
 
-    const datePicker = $("input#mct-date-picker");
-    $(datePicker).on("change", () => {
+    $("button#mct-submit-reminder").on("click", () => {
+      const datePicker = $("input#mct-date-picker");
       const date = datePicker.val() as string;
-      const [year, month, day] = date.split("-").map((val) => Number(val));
-      const newDate = new Date(year, month - 1, day).toLocaleDateString();
-      const reminders: Reminder[] = JSON.parse(
-        localStorage.getItem("mct-reminders") ?? "[]",
-      );
-      reminders.push({
-        targetDate: newDate,
-        courseName: "course",
-        assignmentName: "assignment",
-      });
-      localStorage.setItem("mct-reminders", JSON.stringify(reminders));
+
+      if (date) {
+        const [year, month, day] = date.split("-").map((val) => Number(val));
+        const newDate = new Date(year, month - 1, day).toLocaleDateString();
+
+        const reminders: Reminder[] = JSON.parse(
+          localStorage.getItem("mct-reminders") ?? "[]",
+        );
+        reminders.push({
+          targetDate: newDate,
+          courseName: "course",
+          assignmentName: "assignment",
+        });
+
+        localStorage.setItem("mct-reminders", JSON.stringify(reminders));
+      }
+
+      $("div#mct-input-container").remove();
     });
 
     $("div#mct-grader-close").on("click", () => {
