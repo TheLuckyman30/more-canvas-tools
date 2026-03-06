f54b0e4eac1c287ceb1fadb18f15e51a81c0ba37
TheLuckyman30 <bnauta0@gmail.com>
2026-03-05 23:28:00 -0500

    Merged changes with main

--- src/operations.ts
+++ src/operations.ts
@@ -23,9 +23,6 @@ import { injectModuleIndicator } from "./utilities/module_indicator";
 import { injectGradeReminder } from "./utilities/grade_reminder";
 import { injectAddReminder } from "./utilities/add_reminder";
 import { injectReminderSideBar } from "./utilities/reminder_sidebar";
-import { injectAssignmentIndicator } from "./utilities/add_assignment_indicator";
-
-
 
 const OPERATIONS: ReadonlyArray<Operation<any>> = [
   operation({
