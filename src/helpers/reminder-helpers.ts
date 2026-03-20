a860ac118dfebe2bef7ee2e70249ac1379de6db1
TheLuckyman30 <bnauta0@gmail.com>
2026-03-20 02:15:59 -0400

    Reminders can now be set with a time

--- src/helpers/reminder-helpers.ts
+++ src/helpers/reminder-helpers.ts
@@ -15,15 +15,18 @@ export async function createReminder(
   day: number,
   month: number,
   year: number,
+  hour: number,
+  minute: number,
+  useTime: boolean,
 ) {
-  const newDate = new Date(year, month - 1, day).toLocaleDateString();
+  const newDate = new Date(year, month - 1, day, hour, minute).toLocaleString();
   const newCalendarEvent: CreateCalendarEvent = {
     calendar_event: {
       context_code: `course_${courseId}`,
       title: `Release Grades for ${assignmentName}`,
       description: `Release Grades for ${assignmentName} in ${courseName}`,
-      all_day: true,
-      start_at: `${day}/${month}/${year}`,
+      all_day: !useTime,
+      start_at: `${day}/${month}/${year}T${hour}:${minute}`,
     },
   };
   const nextId = Number(localStorage.getItem("mct-reminder-nextId")) ?? 0;
@@ -54,10 +57,15 @@ export async function createReminder(
 
 export function updateReminder(
   updateReminderId: number,
-  newDate: string,
   calendarId: number,
+  day: number,
+  month: number,
+  year: number,
+  hour: number,
+  minute: number,
+  useTime: boolean,
 ) {
-  const [month, day, year] = newDate.split("/").map((val) => Number(val));
+  const newDate = new Date(year, month - 1, day, hour, minute).toLocaleString();
   const storedReminders: Reminder[] = JSON.parse(
     localStorage.getItem("mct-reminders") ?? "[]",
   );
@@ -70,8 +78,9 @@ export function updateReminder(
 
   const updateCalendarEvent = {
     calendar_event: {
-      start_at: `${day}/${month}/${year}`,
-      end_at: `${day}/${month}/${year}`,
+      all_day: !useTime,
+      start_at: `${day}/${month}/${year}T${hour}:${minute}`,
+      end_at: `${day}/${month}/${year}T${hour}:${minute}`,
     },
   };
 
@@ -115,61 +124,88 @@ export function buildReminderInput(opts: {
 }) {
   const { createOpts, updateOpts } = opts;
   const CLOSE_BUTTON = `
-<div id="mct-grader-close"
-     style="cursor: pointer; font-family: arial; color: #a1a1a1; font-size: 1.5rem">
-  X
-</div>
-`;
+    <div id="mct-grader-close"
+        style="cursor: pointer; font-family: arial; color: #a1a1a1; font-size: 1.5rem">
+    X
+    </div>
+   `;
 
   const DATE_INPUT_HTML = `
-<input id="mct-date-picker" type="date"/>
-`;
+    <input id="mct-date-picker" type="date"/>
+  `;
+
+  const TIME_INPUT_HTML = `
+    <input id="mct-time-picker" type="time"/>
+  `;
 
   const SUBMIT_BUTTON_HTML = `
-<button id="mct-submit-reminder" style="background-color: #bef1c0; padding: 0.5rem; width: 8vw; border-radius: 0.375rem; border: 0px solid #bef1c0;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); cursor: pointer;">
-    Submit
-</button>
-`;
+    <button id="mct-submit-reminder" style="background-color: #bef1c0; padding: 0.5rem; width: 8vw; border-radius: 0.375rem; border: 0px solid #bef1c0;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); cursor: pointer;">
+        Submit
+    </button>
+  `;
 
   const INPUT_CONTAINER_HTML = `
-<div id="mct-input-container"
-    style="position: fixed; height: 100%; width: 100%; backdrop-filter: blur(4px); background-color: rgba(167, 163, 163, 0.2); z-index: 99; top: 0; bottom: 0; right: 0; left: 0; display: flex; justify-content: center; align-items: center">
-    <div style="display: flex; flex-direction: column; width: 20rem; height: 15rem; background-color: #ffffff; box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.14), 0 10px 10px -5px rgba(0, 0, 0, 0.04) ; padding-bottom: 0.5rem; border-radius: 0.375rem;">
-        <div style="display: flex; justify-content: space-between; font-size: 1.5rem; background-color: #bef1c0; padding: 0.5rem; border-radius: 0.375rem 0.375rem 0 0;">
-            <span style= "font-family: arial">${createOpts ? "Set" : "Update"} Your Reminder:</span>
-            ${CLOSE_BUTTON}
-        </div>
-        <div style="display: flex; justify-content: center; align-items: center; height: 100%; width: 100%; font-family: arial; font-size: 1rem; gap: 0.5rem; flex-direction: column; padding: 0.5rem;">
-          Choose Date for Release Reminder:
-          <p></p>
-            ${DATE_INPUT_HTML}
-        </div>
-        <div style="width: 100%; display: flex; justify-content: center">
-            ${SUBMIT_BUTTON_HTML}
+    <div id="mct-input-container"
+        style="position: fixed; height: 100%; width: 100%; backdrop-filter: blur(4px); background-color: rgba(167, 163, 163, 0.2); z-index: 99; top: 0; bottom: 0; right: 0; left: 0; display: flex; justify-content: center; align-items: center">
+        <div style="display: flex; flex-direction: column; width: 20rem; height: 15rem; background-color: #ffffff; box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.14), 0 10px 10px -5px rgba(0, 0, 0, 0.04) ; padding-bottom: 0.5rem; border-radius: 0.375rem;">
+            <div style="display: flex; justify-content: space-between; font-size: 1.5rem; background-color: #bef1c0; padding: 0.5rem; border-radius: 0.375rem 0.375rem 0 0;">
+                <span style= "font-family: arial">${createOpts ? "Set" : "Update"} Your Reminder:</span>
+                ${CLOSE_BUTTON}
+            </div>
+            <div style="display: flex; justify-content: center; align-items: center; height: 100%; width: 100%; font-family: arial; font-size: 1rem; gap: 0.5rem; flex-direction: column; padding: 0.5rem;">
+            Choose Date for Release Reminder:
+            <p></p>
+                ${DATE_INPUT_HTML}
+                ${TIME_INPUT_HTML}
+            </div>
+            <div style="width: 100%; display: flex; justify-content: center">
+                ${SUBMIT_BUTTON_HTML}
+            </div>
         </div>
     </div>
-</div>
-`;
+  `;
 
   $("body").append(INPUT_CONTAINER_HTML);
   $("button#mct-submit-reminder").on("click", async () => {
     const datePicker = $("input#mct-date-picker");
+    const timePicker = $("input#mct-time-picker");
     const date = datePicker.val() as string;
+    let time = timePicker.val() as string;
+    let useTime = true;
+    if (!time) {
+      time = "00:00";
+      useTime = false;
+    }
 
     if (date) {
       const courseId = window.location.pathname.split("/")[2];
       const [year, month, day] = date.split("-").map((val) => Number(val));
+      const [hour, minute] = time.split(":").map((val) => Number(val));
 
       if (createOpts) {
         const { assignmentName, courseName } = createOpts;
-        createReminder(courseId, assignmentName, courseName, day, month, year);
+        createReminder(
+          courseId,
+          assignmentName,
+          courseName,
+          day,
+          month,
+          year,
+          hour,
+          minute,
+          useTime,
+        );
       } else if (updateOpts) {
         const { reminders, index, canDisplayNext, canDisplayPrev } = updateOpts;
-        const newDate = new Date(year, month - 1, day).toLocaleDateString();
         updateReminder(
           reminders[index].id,
-          newDate,
           reminders[index].calendarId,
+          day,
+          month,
+          year,
+          hour,
+          minute,
+          useTime,
         );
         updateOnScreenReminder(
           reminders,
